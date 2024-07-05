f1c4a2eb92d82e7136ff41b2731cab544fe3d654
Lucas <131416070+luc9z@users.noreply.github.com>
2024-07-05 11:12:59 -0300

    config_pastas_iniciais

---
+++ fncontrolapp/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='container'>
+        <div className='Logo'>
+          <h1>FNControl</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
